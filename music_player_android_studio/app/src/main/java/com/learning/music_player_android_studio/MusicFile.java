b2a998a17c00a88ff01bde344f57aa3489d347e9
Yağmur Duran <76050599+elifyagmurduran@users.noreply.github.com>
2022-08-27 16:56:03 +0300

    project upload

---
+++ music_player_android_studio/app/src/main/java/com/learning/music_player_android_studio/MusicFile.java
@@ -0,0 +1,37 @@
+package com.learning.music_player_android_studio;
+
+public class MusicFile {
+    String path;
+    String title;
+    String duration;
+
+    public MusicFile(String path, String title, String duration) {
+        this.path = path;
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+}
